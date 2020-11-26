c04a48cbb9cc196363c9957773fa587b6d784c4d
Andrea Stocco <tsigalko18@gmail.com>
2020-11-26 17:48:58 +0100

    more code on evaluation

--- evaluate_vae.py
+++ evaluate_vae.py
@@ -8,13 +8,12 @@ import numpy as np
 import pandas as pd
 import tensorflow
 from tqdm import tqdm
-from utils import plot_history
 
 import utils
 from config import Config
 from train_vae import setup_vae
 from utils import IMAGE_HEIGHT, IMAGE_WIDTH, RESIZED_IMAGE_HEIGHT, RESIZED_IMAGE_WIDTH, IMAGE_CHANNELS
-from variational_autoencoder import normalize_and_reshape
+from variational_autoencoder import normalize_and_reshape, reshape
 
 np.random.seed(0)
 
@@ -35,7 +34,7 @@ def plot_pictures_orig_rec(orig, dec, picture_name, loss):
     plt.close()
 
 
-def load_or_compute_losses(anomaly_detector, dataset, cached_file_name, delete_cache):
+def load_or_compute_losses(anomaly_detector, encoder, decoder, dataset, cached_file_name, delete_cache):
     losses = []
     current_path = os.getcwd()
     cache_path = os.path.join(current_path, 'cache', cached_file_name)
@@ -54,8 +53,14 @@ def load_or_compute_losses(anomaly_detector, dataset, cached_file_name, delete_c
         i = 0
         for x in tqdm(dataset):
             i = i + 1
+
             x = utils.resize(x)
             x = normalize_and_reshape(x)
+
+            import matplotlib.pyplot as plt
+            plt.imshow(x.reshape(RESIZED_IMAGE_HEIGHT, RESIZED_IMAGE_WIDTH, IMAGE_CHANNELS))
+            plt.show()
+            plt.clf()
 
             # TODO: the version with the VAE loss requires special treatment
             if "VAEloss" in cached_file_name:
@@ -63,16 +68,28 @@ def load_or_compute_losses(anomaly_detector, dataset, cached_file_name, delete_c
             else:
                 loss = anomaly_detector.test_on_batch(x, x)
 
-            # x_rec = anomaly_detector.predict(x)
+            # TODO: the version with the VAE loss requires special treatment (encoder / decoder)
+            encoded = encoder.predict(x)[2]
+            reconstructed = decoder.predict(encoded)
 
-            # import matplotlib.pyplot as plt
-            # plt.imshow(anomaly_detector.predict_on_batch(x).reshape(RESIZED_IMAGE_HEIGHT, RESIZED_IMAGE_WIDTH, IMAGE_CHANNELS))
-            # plt.show()
+            plt.imshow(reconstructed.reshape(RESIZED_IMAGE_HEIGHT, RESIZED_IMAGE_WIDTH, IMAGE_CHANNELS))
+            plt.show()
+            plt.clf()
 
             # if i % 100 == 0:
             #     plot_pictures_orig_rec(x, x_rec, "picture.png", loss)
 
             losses.append(loss)
+
+        # display a 2D plot of the digit classes in the latent space
+        # z_mean, _, _ = encoder.predict(normalize_and_reshape(utils.resize(dataset)))
+        # plt.figure(figsize=(12, 10))
+        # plt.scatter(z_mean[:, 0], z_mean[:, 1])
+        # plt.colorbar()
+        # plt.xlabel("z[0]")
+        # plt.ylabel("z[1]")
+        # plt.show()
+
         np_losses = np.array(losses)
         np.save(cache_path, np_losses)
         print("Losses data for " + cached_file_name + " saved.")
@@ -220,11 +237,19 @@ def compute_losses_vae(cfg, name, images):
         # TODO: do not use .h5 extension when saving/loading custom objects. No longer compatible across platforms!
         model = tensorflow.keras.models.load_model(my_file.__str__())
 
+        encoder_name = name.replace("VAE-", "encoder-")
+        encoder_file = Path(os.path.join(cfg.SAO_MODELS_DIR, encoder_name))
+        encoder = tensorflow.keras.models.load_model(encoder_file.__str__())
+
+        decoder_name = name.replace("VAE-", "decoder-")
+        decoder_file = Path(os.path.join(cfg.SAO_MODELS_DIR, decoder_name))
+        decoder = tensorflow.keras.models.load_model(decoder_file.__str__())
+
     print("Start computing reconstruction losses.")
     start = time.time()
 
     images = images
-    losses = load_or_compute_losses(model, images, name + "-losses.npy", False)
+    losses = load_or_compute_losses(model, encoder, decoder, images, name + "-losses.npy", delete_cache=False)
 
     duration = time.time() - start
     print("Computed reconstruction losses completed in %s." % str(datetime.timedelta(seconds=round(duration))))
@@ -249,14 +274,68 @@ def compute_losses_vae(cfg, name, images):
     return losses
 
 
+def compute_and_plot_all_losses(cfg, use_mse=True):
+    current_path = os.getcwd()
+    if use_mse:
+        cache_path1 = os.path.join(current_path, 'cache', "VAE-track1-MSEloss-allimg-nocrop-losses.npy")
+        cache_path2 = os.path.join(current_path, 'cache', "VAE-track1-MSEloss-allimg-usecrop-losses.npy")
+        cache_path3 = os.path.join(current_path, 'cache', "VAE-track1-MSEloss-centerimg-nocrop-losses.npy")
+        cache_path4 = os.path.join(current_path, 'cache', "VAE-track1-MSEloss-centerimg-usecrop-losses.npy")
+
+        losses1 = np.load(cache_path1).tolist()
+        losses2 = np.load(cache_path2).tolist()
+        losses3 = np.load(cache_path3).tolist()
+        losses4 = np.load(cache_path4).tolist()
+
+        # summarize history for loss
+        plt.figure(figsize=(20, 4))
+        x_losses = np.arange(len(losses1))
+        plt.plot(x_losses, losses1, color='red', alpha=0.7, label="MSEloss-allimg-nocrop")
+        plt.plot(x_losses, losses2, color='blue', alpha=0.7, label="MSEloss-allimg-usecrop")
+        plt.plot(x_losses, losses3, color='green', alpha=0.7, label="MSEloss-centerimg-nocrop")
+        plt.plot(x_losses, losses4, color='black', alpha=0.7, label="MSEloss-centerimg-usecrop")
+    else:
+        cache_path1 = os.path.join(current_path, 'cache', "VAE-track1-VAEloss-allimg-nocrop-losses.npy")
+        cache_path2 = os.path.join(current_path, 'cache', "VAE-track1-VAEloss-allimg-usecrop-losses.npy")
+        cache_path3 = os.path.join(current_path, 'cache', "VAE-track1-VAEloss-centerimg-nocrop-losses.npy")
+        cache_path4 = os.path.join(current_path, 'cache', "VAE-track1-VAEloss-centerimg-usecrop-losses.npy")
+
+        losses1 = np.load(cache_path1).tolist()
+        losses2 = np.load(cache_path2).tolist()
+        losses3 = np.load(cache_path3).tolist()
+        losses4 = np.load(cache_path4).tolist()
+
+        # summarize history for loss
+        plt.figure(figsize=(20, 4))
+        x_losses = np.arange(len(losses1))
+        plt.plot(x_losses, losses1, color='red', alpha=0.7, label="VAEloss-allimg-nocrop")
+        plt.plot(x_losses, losses2, color='blue', alpha=0.7, label="VAEloss-allimg-usecrop")
+        plt.plot(x_losses, losses3, color='green', alpha=0.7, label="VAEloss-centerimg-nocrop")
+        plt.plot(x_losses, losses4, color='black', alpha=0.7, label="VAEloss-centerimg-usecrop")
+
+    plt.ylabel('Loss')
+    plt.xlabel('Frames')
+    plt.title("Reconstruction error for all VAE loss-based anomaly detectors")
+    plt.legend()
+
+    if use_mse:
+        plt.savefig('plots/reconstruction-plot-all-MSE.png')
+    else:
+        plt.savefig('plots/reconstruction-plot-all-VAE.png')
+
+    plt.show()
+
+
 def load_and_eval_vae(cfg, data):
     vae, name = setup_vae(cfg)
 
-    history = np.load(Path(os.path.join(cfg.SAO_MODELS_DIR, name)).__str__() + ".npy", allow_pickle=True).item()
-
-    plot_history(history, cfg, name, vae)
+    # history = np.load(Path(os.path.join(cfg.SAO_MODELS_DIR, name)).__str__() + ".npy", allow_pickle=True).item()
+    # plot_history(history, cfg, name, vae)
 
     losses = compute_losses_vae(cfg, name, data)
+
+    # compute_and_plot_all_losses(cfg, use_mse=True)
+
     # draw_best_worst_results(data, name, losses, "picture_name", numb_of_picture=10)
 
 
