8af08f795a4d3d612b110864747f5016f2de3aab
Anonymous <Anonymous>
2020-11-30 12:31:13 +0100

    fixes to the drive.py to make it compliant w/ the driving quality metrics sim

--- drive.py
+++ drive.py
@@ -1,27 +1,31 @@
 import argparse
 import base64
-import logging
-import os
+import logging, os
 from datetime import datetime
-from io import BytesIO
-from warnings import simplefilter
-
-import eventlet.wsgi
-import numpy as np
-import socketio
-import tensorflow as tf
-from PIL import Image
-from flask import Flask
-
-import utils
-from utils import rmse
-from variational_autoencoder import VariationalAutoencoder
+import shutil
+import csv
 
 logging.disable(logging.WARNING)
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
+# import warnings filter
+from warnings import simplefilter
+
 # ignore all future warnings
 simplefilter(action='ignore', category=FutureWarning)
+
+import numpy as np
+
+import socketio
+import eventlet.wsgi
+from PIL import Image
+from flask import Flask
+from io import BytesIO
+
+from tensorflow.keras.models import load_model
+import utils
+from utils import rmse
+from variational_autoencoder import VariationalAutoencoder
 
 sio = socketio.Server()
 app = Flask(__name__)
@@ -81,15 +85,17 @@ def telemetry(sid, data):
 
         try:
             image = np.asarray(image)  # from PIL image to numpy array
-            image_copy = np.copy(image)
 
+            image_copy = np.copy(image)
+            # image_copy = utils.preprocess(image_copy)  # apply the preprocessing
             image_copy = autoenconder_model.normalize_and_reshape(image_copy)
             loss = anomaly_detection.test_on_batch(image_copy, image_copy)
 
-            image = utils.preprocess(image)  # apply the pre-processing
+            image = utils.preprocess(image)  # apply the preprocessing
             image = np.array([image])  # the model expects 4D array
+            image = image.astype('float32')
 
-            # predict the steering angle for the given image
+            # predict the steering angle for the image
             steering_angle = float(model.predict(image, batch_size=1))
 
             # lower the throttle as the speed increases
@@ -104,7 +110,7 @@ def telemetry(sid, data):
 
             if loss > args.threshold * 1.1:
                 confidence = -1
-            elif args.threshold < loss <= args.threshold * 1.1:
+            elif loss > args.threshold and loss <= args.threshold * 1.1:
                 confidence = 0
             else:
                 confidence = 1
@@ -113,7 +119,7 @@ def telemetry(sid, data):
 
             global frame_id
 
-            send_control(steering_angle, throttle, confidence, loss, args.max_laps)
+            send_control(steering_angle, throttle, brake, confidence, loss, args.max_laps)
             if args.data_dir:
                 csv_path = os.path.join(args.data_dir, args.sim_name)
                 utils.writeCsvLine(csv_path,
@@ -129,21 +135,23 @@ def telemetry(sid, data):
             print(e)
 
     else:
-        sio.emit('manual', data={}, skip_sid=True)  # DO NOT CHANGE THIS
+        # NOTE: DON'T EDIT THIS.
+        sio.emit('manual', data={}, skip_sid=True)
 
 
-@sio.on('connect')  # DO NOT CHANGE THIS
+@sio.on('connect')
 def connect(sid, environ):
     print("connect ", sid)
-    send_control(0, 0, 1, 0, 1)
+    send_control(0, 0, 0, 1, 0, 1)
 
 
-def send_control(steering_angle, throttle, confidence, loss, max_laps):  # DO NOT CHANGE THIS
+def send_control(steering_angle, throttle, brake, confidence, loss, max_laps):
     sio.emit(
         "steer",
         data={
             'steering_angle': steering_angle.__str__(),
             'throttle': throttle.__str__(),
+            'brake': brake.__str__(),
             'confidence': confidence.__str__(),
             'loss': loss.__str__(),
             'max_laps': max_laps.__str__()
@@ -151,18 +159,63 @@ def send_control(steering_angle, throttle, confidence, loss, max_laps):  # DO NO
         skip_sid=True)
 
 
+def load_autoencoder(model):
+    autoencoder = model._create_keras_model()
+    autoencoder.load_weights(args.anomaly_detector_name)
+    assert (autoencoder is not None)
+    return autoencoder
+
+
+def writeCsvLine(filename, row):
+    if filename is not None:
+        filename += "/driving_log.csv"
+        with open(filename, mode='a') as result_file:
+            writer = csv.writer(result_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL,
+                                lineterminator='\n')
+            writer.writerow(row)
+            result_file.flush()
+            result_file.close()
+    else:
+        create_csv_results_file_header(filename)
+
+
+def create_csv_results_file_header(file_name):
+    if file_name is not None:
+        file_name += "/driving_log.csv"
+        with open(file_name, mode='w', newline='') as result_file:
+            csv.writer(result_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+            fieldnames = ["FrameId", "Self Driving Model", "Anomaly Detector", "Threshold", "Track Name", "Lap Number",
+                          "Check Point", "Loss", "Steering Angle", "Throttle", "Brake", "Speed", "Crashed", "center",
+                          "Tot OBEs", "Tot Crashes"]
+            writer = csv.DictWriter(result_file, fieldnames=fieldnames)
+            writer.writeheader()
+            result_file.flush()
+            result_file.close()
+
+    return None
+
+
 if __name__ == '__main__':
+
+    with open('C:\\Users\\41763\\repos\\master-thesis-marco-calzana\\self-driving-car\\model_name.txt',
+              'r') as modelNameFile:
+        fileName = modelNameFile.read()
+    modelNameFile.close()
+
+    print(fileName)
 
     parser = argparse.ArgumentParser(description='Remote Driving - Data Collection')
     parser.add_argument('-d', help='data save directory', dest='data_dir', type=str,
-                        default='simulations')
-    parser.add_argument('-n', help='simulation name', dest='sim_name', type=str, default='trial')
+                        default="simulations")
     parser.add_argument('-m', help='path to the model', dest='model', type=str,
-                        default="model.h5")
+                        default="models/epoch-dataset5-304.h5")
+                        # default='C:\\Users\\41763\\track1_epochmodels\\'+ fileName)
+
     parser.add_argument('-ad', help='path to the anomaly detector model', dest='anomaly_detector', type=str,
                         default="sao/VAE-ICSE20.h5")  # DO NOT CHANGE THIS
     parser.add_argument('-threshold', help='threshold for the outlier detector', dest='threshold', type=float,
                         default=0.035)
+    parser.add_argument('-n', help='simulation name', dest='sim_name', type=str, default='trial')
     parser.add_argument('-s', help='speed', dest='speed', type=int, default=30)
     parser.add_argument('-max_laps', help='number of laps in a simulation', dest='max_laps', type=int, default=1)
 
@@ -175,23 +228,34 @@ if __name__ == '__main__':
         print('{:<20} := {}'.format(key, value))
     print('-' * 30)
 
-    path = os.path.join(os.getcwd(), 'models', args.model)
-
-    if "chauffeur" in args.model:
-        model = tf.keras.models.load_model(path, custom_objects={"rmse": rmse})
-    else:
-        model = tf.keras.models.load_model(path)
-
     MAX_SPEED = args.speed
     MIN_SPEED = 10
     speed_limit = MAX_SPEED
+
+    if "chauffeur" in args.model:
+        model = load_model(args.model, custom_objects={"rmse": rmse})
+        MAX_SPEED += 5
+        speed_limit = MAX_SPEED
+    else:
+        model = load_model(args.model)
+        MAX_SPEED += 5
+        speed_limit = MAX_SPEED
 
     autoenconder_model = VariationalAutoencoder(args.anomaly_detector)
     anomaly_detection = utils.load_autoencoder(autoenconder_model)
     anomaly_detection.compile(optimizer='adam', loss='mean_squared_error')
 
     if args.data_dir != '':
-        utils.create_output_dir(args, utils.csv_fieldnames_improved_simulator)
+        path = os.path.join(args.data_dir, args.sim_name, "IMG")
+        csv_path = os.path.join(args.data_dir, args.sim_name)
+
+        if os.path.exists(path):
+            print("Deleting existing image folder at {}".format(path))
+            shutil.rmtree(csv_path)
+
+        print("Creating image folder at {}".format(path))
+        os.makedirs(path)
+        create_csv_results_file_header(csv_path)
         print("RECORDING THIS RUN ...")
     else:
         print("NOT RECORDING THIS RUN ...")
