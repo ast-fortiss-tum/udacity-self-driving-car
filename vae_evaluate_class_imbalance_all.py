3d35ceebb850ccb388a7ad3854167406f9c4b215
tsigalko18 <tsigalko18@gmail.com>
2021-01-29 12:32:04 +0100

    fixes

--- vae_evaluate_class_imbalance_all.py
+++ vae_evaluate_class_imbalance_all.py
@@ -5,19 +5,22 @@ if __name__ == '__main__':
     cfg = Config()
     cfg.from_pyfile("config_my.py")
 
-    # latent_space = [2, 16]
+    # latent_space = [2]
     # loss_func = ["MSE", "VAE"]
-    # only_center_images = [True, False]
+    # use_only_center_image = [True, False]
     # use_crop = [False, True]
 
-    latent_space = [8]
-    loss_func = ["MSE", "VAE"]
-    use_crop = [False, True]
+    latent_space = [2]
+    loss_func = ["MSE"]
+    use_only_center_image = [True]
+    use_crop = [False]
 
     for ld in latent_space:
         cfg.SAO_LATENT_DIM = ld
         for loss in loss_func:
             cfg.LOSS_SAO_MODEL = loss
-            for crop in use_crop:
-                cfg.USE_CROP = crop
-                evaluate_class_imbalance(cfg)
+            for input_image in use_only_center_image:
+                cfg.USE_ONLY_CENTER_IMG = input_image
+                for crop in use_crop:
+                    cfg.USE_CROP = crop
+                    evaluate_class_imbalance(cfg)
--- vae_train_all.py
+++ vae_train_all.py
@@ -1,16 +1,17 @@
 import utils
 from config import Config
 from utils_vae import load_data_for_vae_training, load_vae
+from vae_evaluate_class_imbalance import evaluate_class_imbalance
 from vae_train import train_vae_model
 
 if __name__ == '__main__':
     cfg = Config()
     cfg.from_pyfile("config_my.py")
 
-    latent_space = [2]
-    loss_func = ["MSE"]
-    use_only_center_image = [True]
-    use_crop = [False]  # True gives poor results
+    latent_space = [8, 16]
+    loss_func = ["MSE", "VAE"]
+    use_only_center_image = [True, False]
+    use_crop = [False, True]
 
     for ld in latent_space:
         cfg.SAO_LATENT_DIM = ld
@@ -27,3 +28,13 @@ if __name__ == '__main__':
                     vae, name = load_vae(cfg, load_vae_from_disk=False)
                     train_vae_model(cfg, vae, name, x_train, x_test, delete_model=True, retraining=False,
                                     sample_weights=None)
+
+    # for ld in latent_space:
+    #     cfg.SAO_LATENT_DIM = ld
+    #     for loss in loss_func:
+    #         cfg.LOSS_SAO_MODEL = loss
+    #         for input_image in use_only_center_image:
+    #             cfg.USE_ONLY_CENTER_IMG = input_image
+    #             for crop in use_crop:
+    #                 cfg.USE_CROP = crop
+    #                 evaluate_class_imbalance(cfg)
